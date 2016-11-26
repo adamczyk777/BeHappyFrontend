4f44668822783affde2aeb06705ea80b820bf1ca
Jakub Adamczyk <adamczyk777@gmail.com>
2016-11-26 11:31:05 +0100

    change to bower, add angular-auto-validate, in progress

--- public_html/js/register.js
+++ public_html/js/register.js
@@ -1,10 +1,18 @@
-var app = angular.module('minmax', []);
+var app = angular.module('minmax', [
+    'jcs-auto-validate'
+]);
 
-app.controller('MinMaxCtrl', function ($scope) {
+app.controller('MinMaxCtrl', function ($scope, $http) {
     $scope.formModel = {};
 
     $scope.onSubmit = function () {
         console.log("Hey i'm submitted");
         console.log($scope.formModel);
+
+        $http.post('url', $scope.formModel).success(function (data) {
+                console.log('submitted');
+            }).error(function (data) {
+                console.log('error');
+        });
     };
 });
