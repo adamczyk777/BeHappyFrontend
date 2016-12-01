6705b3549a1d5ec8ca36a3b8c5cf9a74a5c86acf
Dawid Tomasiewicz <datom2912@interia.pl>
2016-12-01 12:03:36 +0100

    Add angular to app, connect it to therapy and view (show and hide) mock list of therapies.

---
+++ app/therapy/therapy.js
@@ -0,0 +1,38 @@
+(function(){
+    var app = angular.module('therapy', [ ]);
+
+    app.controller('TherapyController', function(){
+        this.products = therapies;
+    });
+
+    var therapies = [
+        {
+            name: 'First therapy lorem',
+            id: 1
+        },
+        {
+            name: 'Second therapy ipsum',
+            id: 2
+        },
+        {
+            name: 'Third therapy dolor',
+            id: 3
+        },
+    ]
+
+    var test = 'Lorem ipsum test variable for controllers';
+
+    app.controller("PanelController", function(){
+        this.tab = 2;   //FIXME if possible change it into ng-init
+
+        this.selectTab = function(setTab){
+            this.tab = setTab;
+        }
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        }
+
+    })
+
+})();
+
