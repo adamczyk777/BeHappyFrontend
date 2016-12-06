3ff7c2870b140652d18e73c11d59792dc1780827
Jakub Adamczyk <adamczyk777@gmail.com>
2016-12-06 22:51:32 +0100

    minor fixes

--- app/register/register.js
+++ app/register/register.js
@@ -4,14 +4,17 @@ angular.module('main').controller('registerCtrl', function ($scope, $http) {
     $scope.onSubmit = function () { //kiedy nacisniemy przycisk submit
 
         console.log("Hey i'm submitted!"); //info ze nacisniety
-        console.log($scope.formModel); //wyswietla w konsoli co polecialo
+        // console.log($scope.formModel); //wyswietla w konsoli co polecialo
 
         $http.post("http://localhost:8080/api/v1/user", $scope.formModel). // wysyla to co widzielismy przed chwila
-        success(function (data) { //log kiedy sukces w wysylaniu
-            console.log("submitted to the server!")
-        }).error(function(data) { //log kiedy sie zjebie
-            console.log("there is an error, check if it's not CORS")
-        });
+        then(
+            function successCallback(response) {
+                console.log("submitted to the server!");
+                TokenStorage.store(response.data.token);
+            },
+            function failureCallback(response) {
+                console.log("there is an error, check if it's not CORS")
+            });
 
     };
 });
