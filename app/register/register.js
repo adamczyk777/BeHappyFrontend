be061ac51b7c506b34f339a74b2ecc2acd308e75
Jakub Adamczyk <adamczyk777@gmail.com>
2016-12-13 00:01:58 +0100

    edit names in login and register forms(directives) + fix issues that came out on our presentation

--- app/register/register.js
+++ app/register/register.js
@@ -1,14 +1,15 @@
 angular.module('main').controller('registerCtrl', ['$scope', '$http', "TokenStorage", function ($scope, $http, TokenStorage) {
-    $scope.formModel = {};
+    $scope.registerForm = {};
     $scope.onSubmit = function () {
-        $http.post("http://localhost:8080/api/user/register", $scope.formModel). // wysyla to co widzielismy przed chwila
+        $http.post("http://localhost:8080/api/user/register", $scope.registerForm). // wysyla to co widzielismy przed chwila
         then(
             function successCallback(response) {
-                console.log("submitted to the server!");
+                console.log("Poszło, Uff... xD");
                 TokenStorage.store(response.data.token);
             },
             function failureCallback(response) {
-                console.log("there is an error, check if it's not CORS");
+                console.log("Mamy błąd :/");
+                alert('No cusz, nie działą :C');
                 console.log(response);
             });
 
