17ce82a8f222696abc6ee4a76650c1afa9fbd883
Dawid Tomasiewicz <datom2912@interia.pl>
2016-12-13 13:57:45 +0100

    Trying to connect - still in progress - commit only to push it to repo and show

--- app/therapy/newTherapy.js
+++ app/therapy/newTherapy.js
@@ -4,13 +4,24 @@
 
     app.controller('TestController', ['$scope', '$http', function($scope, $http){
         $scope.formModel = {};
-        $scope.roleModel = {};
 
         $scope.onSubmit = function () {
-            //var data = $.param($scope.formModel);
+            $scope.formModel.beginningDate = "1111-11-11T23:00:00.00"
+          /*  $scope.formModel.beginningDate = $scope.formModel.beginningDate.getUTCFullYear()
+            + "-" + $scope.formModel.beginningDate.getUTCMonth()
+            + "-" + $scope.formModel.beginningDate.getUTCDay() + "T23:00:00.000";*/
+            /*+ $scope.formModel.beginningDate.getUTCHours() + ":"
+            + $scope.formModel.beginningDate.getUTCMinutes() + ":"
+            + $scope.formModel.beginningDate.getUTCSeconds() + ":"
+            + $scope.formModel.beginningDate.getUTCMilliseconds();*/
+
+
+
+            $scope.formModel.role = "THERAPIST";
             $scope.formModel.patientsCount = 0;
             $scope.formModel.therapistsCount = 0;
             $scope.formModel.wardensCount = 0;
+            //$scope.formModel.date = $filter('date')(new Date(), "MM/dd/yyyy")
             if ($scope.formModel.role.localeCompare("WARDEN") == 0){
                 $scope.formModel.wardensCount ++;
             }
@@ -20,20 +31,37 @@
             if ($scope.formModel.role.localeCompare("PATIENT") == 0){
                 $scope.formModel.patientsCount ++;
             }
+
             $http({
                 method: 'POST',
                 url: 'http://localhost:8080/api/therapies',
-                data: $scope.formModel,
+                data: $scope.formModel
                 /*params: {name: $scope.formModel.name,
                          date: $scope.formModel.date,
                          role: $scope.formModel.role}*/
             }).then(function successCallback(response) {
                 alert("Submitted!");
             }, function errorCallback(response) {
-                alert("Http error status code:" + response.status.toString());
+                if (response.status == 409){
+                    alert("Therapy with given name exists for you")
+                }
+                else
+                {
+                    alert("Http error status code:" + response.status.toString());
+                }
             });
 
         };
     }]);
+
+/*    app.filter('timezone', function(){
+
+        return function (val, offset) {
+            if (val != null && val.length > 16) {
+                return val.substring(0, 16)
+            }
+            return val;
+        };
+    });*/
 
 })();
