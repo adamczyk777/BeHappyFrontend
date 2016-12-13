c8116af58f6cb7bdc9157a34f1972858ec61377b
Mikołaj Skawiński <mkjska@gmail.com>
2016-12-13 23:56:58 +0000

    newTherapy.js edited online with Bitbucket

--- app/therapy/newTherapy.js
+++ app/therapy/newTherapy.js
@@ -4,29 +4,11 @@ angular.module('main')
         $scope.roleModel = {};
 
         $scope.onSubmit = function () {
-            //var data = $.param($scope.formModel);
-           /* $scope.formModel.patientsCount = 0;
-            $scope.formModel.therapistsCount = 0;
-            $scope.formModel.wardensCount = 0;
-            if ($scope.formModel.role.localeCompare("WARDEN") == 0){
-                $scope.formModel.wardensCount ++;
-            }
-            if ($scope.formModel.role.localeCompare("THERAPIST") == 0){
-                $scope.formModel.therapistsCount ++;
-            }
-            if ($scope.formModel.role.localeCompare("PATIENT") == 0){
-                $scope.formModel.patientsCount ++;
-            } */
-
             $scope.localFormat = 'YYYY-MM-DD[T]HH:mm:ss';
             $scope.date = moment($scope.formModel.beginningDate).format($scope.localFormat);
              $scope.toSend = {
-                //id: $scope.formModel.id,
                 name: $scope.formModel.name,
                 beginningDate: $scope.date,
-                /*   patientsCount: $scope.formModel.patientsCount,
-                 therapistsCount: $scope.formModel.therapistsCount,
-                 wardensCount: $scope.formModel.wardensCount,*/
                 role: $scope.formModel.role
             };
             alert ($scope.toSend.name);
@@ -36,9 +18,6 @@ angular.module('main')
                 method: 'POST',
                 url: 'http://localhost:8080/api/therapies',
                 data: $scope.toSend
-                /*params: {name: $scope.formModel.name,
-                         date: $scope.formModel.date,
-                         role: $scope.formModel.role}*/
             }).then(function successCallback(response) {
                 alert("Submitted!");
             }, function errorCallback(response) {
