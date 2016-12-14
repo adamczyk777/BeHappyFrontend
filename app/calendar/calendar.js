6fc3ab895dd930b832bdd44d8af4c2146ec9be8b
tay0108 <kjkosecki@gmail.com>
2016-12-14 22:31:02 +0100

    poprawka wysylania ocenki i daty

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -6,20 +6,27 @@ angular.module('main')
         $scope.minDate = new Date($scope.now.getFullYear(), $scope.now.getMonth(), $scope.now.getDate() - 7);
         $scope.maxDate = new Date($scope.now.getFullYear(), $scope.now.getMonth(), $scope.now.getDate());
 
+        $scope.localFormat = 'YYYY-MM-DD';
 
          // obiekt, do którego widok przyczepia dane do wysłania:
         $scope.formModel = {
             date: $scope.now,
-            mark: ""
+            mark: null
         };
 
-        var therapy_id = 1; // tymczasowo, pozniej bedzie zczytywana w zaleznosci na jaka terapie wejdziemy
+        var therapy_id = 0; // tymczasowo, pozniej bedzie zczytywana w zaleznosci na jaka terapie wejdziemy
 
         $scope.submitForm = function() {  // wysyła dane z formularza (nie wiem jak to ma do końca wyglądać)
+            $scope.toSend = {
+                date: $scope.formModel.date,
+                mark: parseInt($scope.formModel.mark)
+            };
+            $scope.toSend.date = moment($scope.formModel.date).format($scope.localFormat);
+
             $http({
                 method: 'POST',
                 url: 'http://localhost:8080/api/v1/stats/' + therapy_id,
-                data: $scope.formModel
+                data: $scope.toSend
             }).then(function successCallback(response) {
                 alert("Submitted!");
             }, function errorCallback(response) {
