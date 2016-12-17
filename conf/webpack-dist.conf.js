716150b1939978b69a7ef93a6c7f348e15ef8885
Jakub Adamczyk <adamczyk777@gmial.com>
2016-12-17 13:48:10 +0100

    Webpack config for fonts fixed

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -42,6 +42,10 @@ module.exports = {
         loaders: [
           'html-loader'
         ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=public/fonts/[name].[ext]'
       }
     ]
   },
