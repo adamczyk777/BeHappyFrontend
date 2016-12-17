74fbc7b6bc4c9f02bbcaf8c28fd9508c12ff0920
Jakub Adamczyk <adamczyk777@gmail.com>
2016-12-17 19:53:13 +0100

    comment out

--- src/modules/auth/controller.js
+++ src/modules/auth/controller.js
@@ -4,22 +4,22 @@ function controller($http, TokenStorage, $log) {
   var vm = this;
   vm.LogIn = function () {
     $log.log('dupa');
-    var config = {};
-    config.headers = {};
-    config.headers["Authorization"] = vm.email + ":" + vm.password;
-    $http.get(
-      "http://localhost:8080/api/user/login",
-      {headers: {Authorization: btoa(vm.email + ":" + vm.password)}}
-    ).then(
-      function onSuccess(response) {
-        $log.log(response.data.token);
-        TokenStorage.store(response.data.token);
-      },
-      function onFailure(response) {
-        $log.log(response);
-        $log.log(TokenStorage.retrieve());
-      }
-    );
+    // var config = {};
+    // config.headers = {};
+    // config.headers["Authorization"] = vm.email + ":" + vm.password;
+    // $http.get(
+    //   "http://localhost:8080/api/user/login",
+    //   {headers: {Authorization: btoa(vm.email + ":" + vm.password)}}
+    // ).then(
+    //   function onSuccess(response) {
+    //     $log.log(response.data.token);
+    //     TokenStorage.store(response.data.token);
+    //   },
+    //   function onFailure(response) {
+    //     $log.log(response);
+    //     $log.log(TokenStorage.retrieve());
+    //   }
+    // );
   };
   vm.SignUp = function () {
 
