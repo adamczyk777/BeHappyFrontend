d55424dd84d32d919479033e95d7c701c98a7022
Jakub Adamczyk <adamczyk777@gmial.com>
2016-12-17 16:43:38 +0100

    add tokestorage func

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ var routesConfig = require('./routes');
 var authController = require('./modules/auth/controller');
 var homeController = require('./modules/home/controller');
 var therapiesService = require('./services/therapies.service');
+var registerController = require('./modules/register/controller');
 
 angular
   .module('app', ['ui.router'])
   .config(routesConfig)
   .controller('AuthController', authController)
   .controller('HomeController', homeController)
-  .factory('TherapiesService', therapiesService);
+  .factory('TherapiesService', therapiesService)
+  .controller('RegisterController', registerController);
