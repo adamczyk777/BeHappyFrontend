10d84a0f05936f14cd31da87bd52f9cd402e7649
Jakub Adamczyk <adamczyk777@gmail.com>
2016-12-20 21:32:03 +0100

    make pass inputs work, binding repaired

--- src/modules/home/controller.js
+++ src/modules/home/controller.js
@@ -1,15 +1,15 @@
 module.exports = controller;
 
-function controller($log, TokenStorage, $http, $scope) {
+function controller($log, TokenStorage, $http) {
   var vm = this;
 
   vm.login = function () {
     var config = {};
     config.headers = {};
-    config.headers.Authorization = $scope.email + ":" + $scope.password;
+    config.headers.Authorization = vm.emailField + ":" + vm.passwordField;
     $http.get(
       "http://localhost:8080/api/user/login",
-      {headers: {Authorization: btoa($scope.email + ":" + $scope.password)}}
+      {headers: {Authorization: btoa(vm.emailField + ":" + vm.passwordField)}}
     ).then(
       function onSuccess(response) {
         $log.log(response.data.token);
