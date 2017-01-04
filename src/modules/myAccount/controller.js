d2e66054b603f02b63651356285a7c9bd15fb9a2
Kamil Kaleta <kamkalet@gmail.com>
2017-01-04 18:08:00 +0100

    added some component, functions

--- src/modules/myAccount/controller.js
+++ src/modules/myAccount/controller.js
@@ -5,7 +5,48 @@ function controller(TokenStorage, $state, $log, $http, api) {
   if (TokenStorage.retrieve() === null) {
     $state.go('app.login');
   }
+// vm = this;
+  $scope.changeAddress = function (newAddress) {
+    $log.log("Trying to change address");
+    $log.log(newAddress);
+    $http({
+      method: 'POST',
+      url: "http://localhost:8080/api//#", // TODO endpoint
+      data: {name: newAddress, id: $scope.}
+    }).then(function successCallback(response) {
+      $log.log("Address changed");
+      $log.log(response);
+    }, function errorCallback(response) {
+      $log.log("Http error status code:" + response.status.toString());
+    });
+  };
+  $scope.changeName = function (newName) {
+    $log.log("Trying to change name");
+    $log.log(newName);
+    $http({
+      method: 'POST',
+      url: "http://localhost:8080/api//#", // TODO endpoint
+      data: {name: newAddress, id: $scope.}
+    }).then(function successCallback(response) {
+      $log.log("Address changed");
+      $log.log(response);
+    }, function errorCallback(response) {
+      $log.log("Http error status code:" + response.status.toString());
+    });
+  };
 
 
-
+  $scope.deleteUser = function () {
+    $log.log("Trying to delete user");
+    $http({
+      method: 'POST',
+      url: "http://localhost:8080/api/user/#", // TODO endpoint
+      data: {id: $scope.therapyId}
+    }).then(function successCallback(response) {
+      $log.log("User deleted");
+      $log.log(response);
+    }, function errorCallback(response) {
+      $log.log("Http error status code:" + response.status.toString());
+    });
+  };
 }
