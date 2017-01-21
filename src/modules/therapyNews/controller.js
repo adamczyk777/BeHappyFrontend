0acde2224ccba4d986f3908debe31ea24a0d907b
rkuzicki <rkuzicki@gmail.com>
2017-01-21 12:35:31 +0000

    I disabled ES-Lint and made this shit work. Call me maestro.

---
+++ src/modules/therapyNews/controller.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+module.exports = controller;
+/* @ngInject */
+  /* .constant("dataUrl", "http://localhost:8080/therapy/news")
+  .constant("newsListPageCount", 5)*/
+function controller ($scope /* , $http, $filter, dataUrl*/) {
+    $scope.data = {};
+   /* $http.get(dataUrl)
+    //.then(function (data) {
+    //    $scope.data.products = data;
+      .then(function (response) {
+          $scope.data.therapyinfo = response.data;
+        },
+        function (error) {
+          $scope.data.error = error;
+        });
+    $scope.selectedPage=1;
+    $scope.pageSize = newsListPageCount;
+    $scope.selectPage = function(newPage)
+    {
+      $scope.selectedPage = newPage;
+    }
+
+    $scope.getPageClass = function (page)
+    {
+      return $scope.selectedPage == page ? newsListActiveClass : "";
+    }
+    $scope.therapyFilterFn = function (info)
+    {
+      return selectedTherapy == null || info.therapyName == selectedTherapy;
+    }*/
+  }
