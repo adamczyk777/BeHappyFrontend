0bd3893af550b01187bde62e8f51dc05b8c4824b
Jakub Adamczyk <adamczyk777@gmail.com>
2017-02-09 15:50:48 +0100

    add timeout for redirecting to login

--- src/modules/confirmation/controller.js
+++ src/modules/confirmation/controller.js
@@ -1,13 +1,15 @@
 module.exports = controller;
 /** @ngInject */
-function controller($log, $state, $http, $stateParams) {
+function controller($log, $state, $http, $stateParams, $timeout) {
   var vm = this;
   vm.id = $stateParams.id;
   $http.get('http://behappy.io:8081/api/users/confirm/' + vm.id)
     .then(
       function successCallback() {
         $log.log('account confirmed!');
-        $state.go('login');
+        $timeout(function () {
+          $state.go('login');
+        }, 5000);
       },
       function failureCallback() {
         $log.log('something is wrong with this :/');
