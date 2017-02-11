9e85b5bec6e29a9be0977a22b791008f546b8104
Jakub Adamczyk <adamczyk777@gmail.com>
2017-02-11 01:41:12 +0100

    add ficzers

--- src/modules/confirmation/controller.js
+++ src/modules/confirmation/controller.js
@@ -2,16 +2,27 @@ module.exports = controller;
 /** @ngInject */
 function controller($log, $state, $http, $stateParams, $timeout) {
   var vm = this;
+  vm.hideCounter = true;
+  vm.message = "";
+  vm.counter = 5;
   vm.id = $stateParams.id;
-  $http.get('http://behappy.io:8081/api/users/confirm/' + vm.id)
+  $http.get('http://localhost:8080/api/users/confirm/' + vm.id)
     .then(
       function successCallback() {
+        vm.hideCounter = false;
+        vm.message = "Rejestracja potwierdzona! :)";
         $log.log('account confirmed!');
-        $timeout(function () {
+        for (var i = 0; i < 5; i++) {
+          $timeout(function () {
+            vm.counter--;
+          }, 1000);
+        }
+        if (vm.counter === 0) {
           $state.go('login');
-        }, 5000);
+        }
       },
       function failureCallback() {
+        vm.message = "Rejestracja nie powiodła się! :(";
         $log.log('something is wrong with this :/');
       }
     );
