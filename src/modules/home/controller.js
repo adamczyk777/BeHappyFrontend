db2f5e0f52e692357d9f114ca78385e9cf8c42ed
mikolajskawinski <mkjska@gmail.com>
2017-02-13 02:13:45 +0100

    Make pagination of news on home page great again

--- src/modules/home/controller.js
+++ src/modules/home/controller.js
@@ -2,7 +2,6 @@ module.exports = controller;
 /** @ngInject */
 function controller($scope, TokenStorage, $state, $log, $http, api) {
   var vm = this;
-
   if (!TokenStorage.isAuthenticated()) {
     $state.go('login');
   }
@@ -21,18 +20,50 @@ function controller($scope, TokenStorage, $state, $log, $http, api) {
       });
   };
 
-  $scope.news = {};
-  $scope.getNews = function () {
+  $scope.chosenPage = 1;
+  $scope.count = 1;
+  $scope.news = [];
+  $scope.number = 1;
+  $scope.firstTime = true;
+
+  $scope.getNews = function (k) {
     $http({
       method: 'GET',
-      url: api.endpoint + '/news'
+      url: api.endpoint + '/news/' + k
     }).then(function successCallback(response) {
-      $scope.news = response.data;
-      $scope.therapyName = $scope.findTherapyName();
-      $log.log($scope.news);
+      $scope.news[k - 1] = {};
+      $scope.news[k - 1] = response.data;
+      $scope.news[k - 1].number = k;
+      $log.log($scope.news[0].description);
+      $log.log("$scope.news[k-1]");
+      $log.log($scope.news[k - 1]);
+      $log.log("NEWS COUNT");
+      $log.log($scope.news[k - 1]);
+      $scope.count = Math.floor(response.data[0].count / 5) + 1;
+      $log.log("RESPONSE DATA COUNT");
+      $log.log(response.data[0].count);
+      $log.log("COUNT");
+      $log.log($scope.count);
+      if ($scope.firstTime) {
+        $scope.setPage($scope.news[0]);
+        for (var j = 2; j <= $scope.count; j++) {
+          $scope.getNews(j);
+        }
+      }
+      $log.log("NEWS");
+      $log.log($scope.news.count);
+      $scope.firstTime = false;
     }, function errorCallback() {
       $log.log("Cannot get data from server.");
     });
   };
-  $scope.getNews();
+  $scope.getNews(1);
+
+  $scope.setPage = function (i) {
+    $scope.chosenPage = i;
+  };
+
+  $scope.isChosen = function (i) {
+    return ($scope.chosenPage === i);
+  };
 }
